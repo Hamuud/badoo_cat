1cebf2ba410e6f67183011cece07e2b2e61dc15e
Hamuud <artemlask2014@gmail.com>
2023-08-21 13:54:26 +0300

    Starter pack

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+import Home from './pages/Home/Home';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
 
 export const App: React.FC = () => {
   return (
     <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+
+        <Route path="*" element={<PageNotFound />} />
+
+      </Routes>
     </div>
   );
 };
